dafdf49bf7b45f285cadf606dd46aef2f12ebc00
skymill2000 <skymill2000@gmail.com>
2021-10-19 13:44:40 +0900

    주석추가

--- scraping/eumPrac.py
+++ scraping/eumPrac.py
@@ -9,4 +9,5 @@ driver.get('https://www.eum.go.kr/web/am/amMain.jsp;')
 sidoSelect = Select(driver.find_element_by_xpath('//*[@id="selSido"]'))
 sidoSelect.select_by_visible_text("전라남도")
 
-# work1 전라남도 고흥군 고흥읍 남계리 45 - 1 번지의 데이터를 스크래핑하기
+# work1 전라남도 고흥군 고흥읍 남계리 45 - 1 번지의 데이터를 스크래핑하기 / 공시지가 가져오기
+# driver.implicitly_wait(1)// 함정을 피할수 있는 방법 1 = 1초동안 대기한다
