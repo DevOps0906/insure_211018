cab950a4f766c27b3544014b053278835407d0f2
skymill2000 <skymill2000@gmail.com>
2021-10-19 13:42:40 +0900

    #work1 토지 이음 사이트 스크래핑 실습

---
+++ scraping/eumPrac.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+driver = webdriver.Chrome('./chromedriver')
+# 크롬을 통해서 스크래핑을 진행 크롬 드라이버 로딩
+driver.get('https://www.eum.go.kr/web/am/amMain.jsp;')
+# naver 페이지 열기
+
+sidoSelect = Select(driver.find_element_by_xpath('//*[@id="selSido"]'))
+sidoSelect.select_by_visible_text("전라남도")
+
+# work1 전라남도 고흥군 고흥읍 남계리 45 - 1 번지의 데이터를 스크래핑하기
--- scraping/loginLol.py
+++ scraping/loginLol.py
@@ -7,3 +7,12 @@ driver.get('https://kr.leagueoflegends.com/ko-kr/')
 loginBtn = driver.find_element_by_xpath(
     '//*[@id="riotbar-right-content"]/div[3]/div[1]/a')
 loginBtn.click()
+
+usernameInput = driver.find_element_by_name("username")
+passwordInput = driver.find_element_by_name("password")
+authBtn = driver.find_element_by_xpath(
+    "/html/body/div/div/div/div[2]/div/div/button")
+
+usernameInput.send_keys("test")
+passwordInput.send_keys("test")
+authBtn.click()
