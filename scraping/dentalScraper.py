ec62e2423ad6fc6bebb5bae6e75880979f42948d
skymill2000 <skymill2000@gmail.com>
2021-10-19 16:12:27 +0900

    치아 보험료 조회 스크래핑 완성

--- scraping/dentalScraper.py
+++ scraping/dentalScraper.py
@@ -1,38 +1,107 @@
-# AIA 생명 치과보험사이트에 정보 입력(내정보) 보험료 / 보장 내역 출력하기;
-# https://www.aia.co.kr/ko/our-products/medical-protection/non-par-denteal-health-plan.html
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+import re
 
-driver = webdriver.Chrome('./chromedriver')
-# 크롬을 통해서 스크래핑을 진행 크롬 드라이버 로딩
-driver.get(
-    'https://www.aia.co.kr/ko/our-products/medical-protection/non-par-denteal-health-plan.html')
 
-maleBtn = driver.find_element_by_xpath(
-    '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[2]')
-maleBtn.click()
+def rePlaceData(value):
+    numbers = re.findall("\d+", value)
+    result = ""
+    for i in numbers:
+        decodedNumber = i
+        result += decodedNumber
+    return result
 
-birthInput = driver.find_element_by_xpath('//*[@id="aia644363719"]')
-birthInput.send_keys("19890119")
+# 실습3
+# AIA 생명 치아보험 견적 페이지에서 보장 내역과 보험료을 가져와서 출력을한다
 
-searchBtn = driver.find_element_by_xpath('//*[@id="btn806817556"]')
-searchBtn.click()
-# naver 페이지 열기
-driver.implicitly_wait(3)
 
-priceText = driver.find_element_by_xpath(
-    '//*[@id="premium-by-timespan-value"]').text
-print(priceText)
+def getAIAData(name, birth, gender):
+    driver = webdriver.Chrome('./chromedriver')
+    scrapingResult = {
+        'company': "AIA",
+        'price': 0,
+        'contents': []
+    }
+    driver.implicitly_wait(3)
+    driver.get(
+        'https://www.aia.co.kr/ko/our-products/medical-protection/non-par-denteal-health-plan.html#')
+    driver.implicitly_wait(3)
+    # name.decode('utf-8').encode('euc-kr')
+    textBox = driver.find_element_by_xpath('//*[@id="aia644363719"]')
+    textBox.send_keys("19"+birth)
+    if gender == 1:
+        femaleBtn = driver.find_element_by_xpath(
+            '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[1]')
+        # '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[1]
 
-resultTable = driver.find_element_by_xpath(
-    '//*[@id="collapse-large--77993452"]/div[2]/table/tbody')
-rows = resultTable.find_elements_by_tag_name('tr')
-tableBody = driver.find_element_by_xpath(
-    '//*[@id="collapse-large-724022276"]/div[1]/div/table').find_element_by_tag_name('tbody')
-driver.find_element_by_xpath(
-    '//*[@id="the_fine_print"]/div[2]/div[1]/div[2]/div/a[2]').click()
-rows = tableBody.find_elements_by_tag_name("tr")
-contentsList = []
-for index, value in enumerate(rows):
-    if index != 0:
-        print(value.find_elements_by_tag_name('td')[0].text)
+        femaleBtn.click()
+    else:
+        maleBtn = driver.find_element_by_xpath(
+            '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[2]')
+        maleBtn.click()
+    resultBtn = driver.find_element_by_xpath('//*[@id="btn806817556"]')
+    resultBtn.click()
+    driver.implicitly_wait(3)
+
+    htmlResult = driver.find_element_by_xpath(
+        '//*[@id="premium-by-timespan-value"]').text
+    resultValue = rePlaceData(htmlResult)
+    print(resultValue)
+    scrapingResult['price'] = resultValue
+    tableBody = driver.find_element_by_xpath(
+        '//*[@id="collapse-large-724022276"]/div[1]/div/table').find_element_by_tag_name('tbody')
+    driver.find_element_by_xpath(
+        '//*[@id="the_fine_print"]/div[2]/div[1]/div[2]/div/a[2]').click()
+    rows = tableBody.find_elements_by_tag_name("tr")
+    contentsList = []
+    for index, value in enumerate(rows):
+        if index != 0:
+            print(value.find_elements_by_tag_name('td')[0].text)
+            contentsList.append(value.find_elements_by_tag_name('td')[0].text)
+    scrapingResult['contents'] = contentsList
+    return scrapingResult
+
+
+def getLinaData(name, birth, gender):
+    driver = webdriver.Chrome('./chromedriver')
+    scrapingResult = {
+        'company': "라이나",
+        'price': 0,
+        'contents': []
+    }
+    driver.get('https://direct.lina.co.kr/product/ess/dtc01/easy')
+    textBox = driver.find_element_by_xpath('//*[@id="birthday"]')
+    textBox.send_keys(birth)
+    if gender == 1:
+        femaleBtn = driver.find_element_by_xpath('//*[@id="main_btn_female"]')
+        femaleBtn.click()
+    else:
+        maleBtn = driver.find_element_by_xpath('//*[@id="main_btn_male"]')
+        maleBtn.click()
+    resultBtn = driver.find_element_by_xpath(
+        '//*[@id="btn_direct_dental_cal"]')
+    resultBtn.click()
+    driver.implicitly_wait(3)
+
+    htmlResult = driver.find_element_by_xpath(
+        '//*[@id="contents"]/div[2]/div[2]/div[2]/div/table/tbody[1]/tr[1]/td[2]/strong').text
+    resultValue = rePlaceData(htmlResult)
+    scrapingResult['price'] = resultValue
+    driver.implicitly_wait(2)
+    detailBtn = driver.find_element_by_xpath('//*[@id="openLayerplanPonA2"]')
+    detailBtn.click()
+    driver.implicitly_wait(2)
+
+    tableBody = driver.find_element_by_xpath(
+        '//*[@id="planPonA2"]/div/div[2]/div/div/table[1]').find_element_by_tag_name('tbody')
+    rows = tableBody.find_elements_by_tag_name("tr")
+    contentsList = []
+    for index, value in enumerate(rows):
+        if index != 0:
+            print(value.find_elements_by_tag_name('th')[0].text)
+            contentsList.append(value.find_elements_by_tag_name('th')[0].text)
+    scrapingResult['contents'] = contentsList
+    return scrapingResult
+
+
+print(getLinaData("유관우", "890119", 0))
+print(getAIAData("유관우", "890119", 0))
