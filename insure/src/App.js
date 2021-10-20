da97831ddc3b59bef26981ca7b0e4b1a8c9c5d47
skymill2000 <skymill2000@gmail.com>
2021-10-20 16:07:30 +0900

    라우터 테스트

--- insure/src/App.js
+++ insure/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import DesignComponent from "./commponents/DesignComponent";
+import ListComponent from "./commponents/ListComponent";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/test" exact></Route>
+        <Route path="/test" exact>
+          <DesignComponent></DesignComponent>
+        </Route>
+        <Route path="/list" exact>
+          <ListComponent></ListComponent>
+        </Route>
       </Switch>
     </Router>
   );
