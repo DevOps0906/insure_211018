d21acbe17337ad8d95cede42b07f7e1de82276a8
skymill2000 <skymill2000@gmail.com>
2021-10-20 11:16:27 +0900

    배열 선언

---
+++ javascript_basic/arrayExample.js
@@ -0,0 +1,15 @@
+//in java
+// public String [] stringArray = new String[4];
+let car = {
+  name: "g80",
+  ph: 500,
+  start: function () {
+    console.log("엔진 시작");
+  },
+  stop: function () {
+    console.log("엔진 정지");
+  },
+};
+
+let cars = ["bmw", "hyundai", "volvo", "lexus", 5, 2, car];
+console.log(cars);
