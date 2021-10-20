34065638b144ffeea78a5f9ea0b83f211dcac005
skymill2000 <skymill2000@gmail.com>
2021-10-20 14:50:31 +0900

    클릭 이벤트 바인딩

--- insure/src/commponents/StateExample.js
+++ insure/src/commponents/StateExample.js
@@ -4,14 +4,19 @@ const StateExample = () => {
   const [title, setTitle] = useState("초기 문자");
   const handleChange = (e) => {
     const { value, name } = e.target;
-    console.log(value);
+    console.log(name);
     setTitle(value);
+  };
+
+  const handleClick = () => {
+    alert(title);
   };
 
   return (
     <div>
       <p>{title}</p>
-      <input onChange={handleChange}></input>
+      <input onChange={handleChange} name="contents"></input>
+      <button onClick={handleClick}>전송 버튼</button>
     </div>
   );
 };
