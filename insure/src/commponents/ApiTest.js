98dfe3c637b0fa1e9e22e0b3b5010ae1ebed82c2
skymill2000 <skymill2000@gmail.com>
2021-10-21 11:06:26 +0900

    뉴스 api 제목 표시 완성

--- insure/src/commponents/ApiTest.js
+++ insure/src/commponents/ApiTest.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const ApiTest = () => {
+  const [newsData, setnewsData] = useState([]);
   const handleClick = () => {
     console.log("버튼을 클릭했습니다");
     let apiUrl =
       "https://newsapi.org/v2/everything?qInTitle=애플&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5";
     //여러분들의 URL
-    axios.get(apiUrl).then((response) => {
-      console.log(response);
+    axios.get(apiUrl).then(({ data }) => {
+      const newsArticles = data.articles;
+      console.log(data.articles);
+      setnewsData(newsArticles);
     });
   };
 
   return (
     <div>
       <button onClick={handleClick}>데이터가져오기</button>
+      {newsData.map((news) => {
+        return <p>{news.title}</p>;
+      })}
     </div>
   );
 };
