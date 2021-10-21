21e8f633cc1949e21faaec6e8623d58126764108
skymill2000 <skymill2000@gmail.com>
2021-10-21 10:40:44 +0900

    최초의 axios request 생성

--- insure/src/commponents/ApiTest.js
+++ insure/src/commponents/ApiTest.js
@@ -1,8 +1,15 @@
 import React from "react";
+import axios from "axios";
 
 const ApiTest = () => {
   const handleClick = () => {
     console.log("버튼을 클릭했습니다");
+    let apiUrl =
+      "https://newsapi.org/v2/everything?qInTitle=애플&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5";
+    //여러분들의 URL
+    axios.get(apiUrl).then((response) => {
+      console.log(response);
+    });
   };
 
   return (
