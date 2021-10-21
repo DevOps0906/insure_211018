809ea2abc31f9387d42f1f24a993b78b56ac7068
skymill2000 <skymill2000@gmail.com>
2021-10-21 16:25:41 +0900

    병원 검색 기능 / 리스트 표시 기능 완성

---
+++ insure/src/page/Hospital.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import React, { useState } from "react";
+import HospitalListItem from "../commponents/Hospital/HospitalListItem";
+import Header from "../commponents/News/Header";
+
+const Hospital = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [hospitalList, setHospitalList] = useState([]);
+
+  const getHospitalList = () => {
+    const option = {
+      method: "GET",
+      url: "http://apis.data.go.kr/B551182/hospInfoService1/getHospBasisList1",
+      headers: {},
+      params: {
+        ServiceKey:
+          "uiu3ZzNzDB04UbxOtOL1atH04WOtxB5WSKkPbaCASVHbwgcsIPwHA5Qp6xOmSe6fzCnUVifZcfTXDkgNegv4qQ==",
+        pageNo: "1",
+        numOfRows: "10",
+        clCd: "31",
+        yadmNm: searchInput,
+      },
+    };
+    axios(option).then((response) => {
+      console.log(response.data);
+      setHospitalList(response.data.response.body.items.item);
+    });
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchInput(value);
+  };
+
+  return (
+    <>
+      <Header title="병원목록 조회"></Header>
+      <input onChange={handleChange}></input>
+      <button onClick={getHospitalList}>병원 조회하기</button>
+      {hospitalList.map(({ addr, clCd, clCdNm, yadmNm, ykiho }) => {
+        return (
+          <HospitalListItem
+            key={ykiho}
+            addr={addr}
+            clCd={clCd}
+            clCdNm={clCdNm}
+            yadmNm={yadmNm}
+            ykiho={ykiho}
+          ></HospitalListItem>
+        );
+      })}
+    </>
+  );
+};
+
+export default Hospital;
