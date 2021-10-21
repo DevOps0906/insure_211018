b8f2d1b8fcb10592cb29f0a5d776c250af65ceb9
skymill2000 <skymill2000@gmail.com>
2021-10-21 11:45:54 +0900

    뉴스 검색 버튼 클릭 로직 완성

--- insure/src/page/NewsPage.js
+++ insure/src/page/NewsPage.js
@@ -1,14 +1,29 @@
+import axios from "axios";
 import React, { useState } from "react";
 import Header from "../commponents/News/Header";
 import Search from "../commponents/News/Search";
 
 const NewsPage = () => {
   const [queryInput, setqueryInput] = useState("");
+  const [newsData, setnewsData] = useState([]);
+
   const handleQueryInput = (e) => {
     const { value } = e.target;
     setqueryInput(value);
     console.log(queryInput);
   };
+
+  const handleSearchBtn = () => {
+    let apiUrl =
+      "https://newsapi.org/v2/everything?qInTitle=애플&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5";
+    //여러분들의 URL
+    axios.get(apiUrl).then(({ data }) => {
+      const newsArticles = data.articles;
+      console.log(data.articles);
+      setnewsData(newsArticles);
+    });
+  };
+
   return (
     <div>
       <Header title="뉴스 검색"></Header>
